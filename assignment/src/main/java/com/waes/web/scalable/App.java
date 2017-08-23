8ed3e6f8cf6536e46520386621ac8279e85ffff6
Leonardo Yoshiwara <leonardo_yoshiwara@gap.com>
2017-08-23 12:39:15 -0300

    Initial commit

---
+++ assignment/src/main/java/com/waes/web/scalable/App.java
@@ -0,0 +1,13 @@
+package com.waes.web.scalable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
